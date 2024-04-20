371b2f1761eb7300995918a3bbdead4e30ec4505
Bayvao <vermabayvao@gmail.com>
2024-04-20 10:43:02 +0530

    respond to multiple PING

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -20,9 +22,23 @@ public class Main {
           serverSocket.setReuseAddress(true);
     //      // Wait for connection from client.
           clientSocket = serverSocket.accept();
+            var br = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
             OutputStream outputStream = clientSocket.getOutputStream();
-            outputStream.write("+PONG\r\n".getBytes(StandardCharsets.UTF_8));
-            outputStream.flush();
+            String command;
+            while ((command = br.readLine()) != null) {
+                System.out.println("command: " + command);
+                if (command.equalsIgnoreCase("ping")) {
+                    outputStream.write("+PONG\r\n".getBytes(StandardCharsets.UTF_8));
+                    // clientSocket.close();
+                }
+            }
+            clientSocket.close();
+            // OutputStream outputStream = clientSocket.getOutputStream();
+            // outputStream.write("+PONG\r\n".getBytes(StandardCharsets.UTF_8));
+            // clientSocket.close();
+            //  }
+            
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
