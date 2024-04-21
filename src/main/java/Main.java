87e6dd4b630136858ee63a87ce0cb46da6b550e5
Bayvao <vermabayvao@gmail.com>
2024-04-21 11:28:29 +0530

    COnfiguring listening on custom port

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args){
         int port = 6379;
 
-        if ("--port".equalsIgnoreCase(args[0])) {
+        if (args.length > 1 && "--port".equalsIgnoreCase(args[0])) {
             port = Integer.parseInt(args[1]);
         }
 
