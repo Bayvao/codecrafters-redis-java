ad02fbaab882f43c3b149d93a441c7afcded0bac
Bayvao <vermabayvao@gmail.com>
2024-04-21 10:06:11 +0530

    Implemented SET and GET Command

--- src/main/java/CommandHandler.java
+++ src/main/java/CommandHandler.java
@@ -1,14 +1,27 @@
 public class CommandHandler {
-  private static final String CRLF_TERMINATOR = "\r\n";
-  private CommandHandler() {}
-  public static String handle(String parsedCommand) {
-    String[] arguments = parsedCommand.split(" ");
-    String command = arguments[0].toLowerCase();
-    return switch (command) {
+
+    private static final String CRLF_TERMINATOR = "\r\n";
+    private CommandHandler() {
+    }
+    public static String handle(String parsedCommand) {
+        String[] arguments = parsedCommand.split(" ");
+        String command = arguments[0].toLowerCase();
+        return switch (command) {
             case "ping" -> "+PONG\r\n";
             case "echo" -> "$" + arguments[1].length() + CRLF_TERMINATOR + arguments[1] + CRLF_TERMINATOR;
+            case "set" -> setCommandData(arguments);
+            case "get" -> getCommandData(arguments);
             default -> throw new RuntimeException("Unknown command: " + command);
         };
+    }
 
+    private static String setCommandData(String[] arguments) {
+        DataModel dataModel = new DataModel();
+        return dataModel.setSetCommandMap(arguments[1], arguments[2]);
+    }
+
+    private static String getCommandData(String[] arguments) {
+        DataModel dataModel = new DataModel();
+        return dataModel.getSetCommandMap(arguments[1]);
     }
 }
---
+++ src/main/java/DataModel.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DataModel {
+    private ConcurrentHashMap<Object, String> setCommandMap = new ConcurrentHashMap<>();
+
+    public String getSetCommandMap(String key) {
+        return setCommandMap.get(key);
+    }
+
+    public String setSetCommandMap(Object key, String value) {
+        this.setCommandMap.put(key, value);
+        return "OK";
+    }
+}
