ad1e6272905d6c3052a992740be068c2f82ee025
Bayvao <vermabayvao@gmail.com>
2024-04-24 00:29:34 +0530

    data replication

--- src/main/java/Slave.java
+++ src/main/java/Slave.java
@@ -50,7 +50,6 @@ public class Slave {
                     serverWriter.write(getPsyncConfBytes(serverInformation));
                 }
             }
-            serverWriter.flush();
         }
     }
 
