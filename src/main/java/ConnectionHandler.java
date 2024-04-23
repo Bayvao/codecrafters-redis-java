71d7dc18db5a4f1234de1f37e10d89bac741c7c1
Bayvao <vermabayvao@gmail.com>
2024-04-23 13:17:06 +0530

    Handled NPE

--- src/main/java/ConnectionHandler.java
+++ src/main/java/ConnectionHandler.java
@@ -33,8 +33,8 @@ public class ConnectionHandler extends Thread {
         outputStream.write(response.getBytes(StandardCharsets.UTF_8));
 
         if(response.contains("FULLRESYNC")) {
-          outputStream.write(sendEmptyRDBFile());
           serverInformation.setReplicaSet(outputStream);
+          outputStream.write(sendEmptyRDBFile());
         }
       }
     } catch (EOFException e) {
